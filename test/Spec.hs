577441d2861debcc6f24ed3258b7c4100190e29d
0x11901 <wjk930726@live.com>
2019-02-28 16:36:30 +0800

    Give me a break, it's 2am. But it works now.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,6 @@
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+replicate' :: Int -> a -> [a]
+replicate' n x | n <= 0    = []
+               | otherwise = x : replicate' (n - 1) x
