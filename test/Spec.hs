061ea732437451ec07767e9b54139339c15617f2
0x11901 <wjk930726@live.com>
2019-02-28 17:22:22 +0800

    I don't get paid enough for this shit.

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,3 +9,17 @@ take' :: (Num i, Ord i) => i -> [a] -> [a]
 take' _ [] = []
 take' n (x : xs) | n <= 0    = []
                  | otherwise = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' []       = []
+reverse' (x : xs) = reverse' xs ++ [x]
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' []       _        = []
+zip' _        []       = []
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x : xs) | a == x    = True
+                 | otherwise = a `elem'` xs
