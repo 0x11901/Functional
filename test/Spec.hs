2248eb8f8039dd968ad737a0e68ff750d9215e69
0x11901 <wjk930726@live.com>
2019-02-28 16:43:39 +0800

    this should fix it

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,3 +4,8 @@ main = putStrLn "Test suite not yet implemented"
 replicate' :: Int -> a -> [a]
 replicate' n x | n <= 0    = []
                | otherwise = x : replicate' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' _ [] = []
+take' n (x : xs) | n <= 0    = []
+                 | otherwise = x : take' (n - 1) xs
