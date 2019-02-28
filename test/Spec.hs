9632406d487db2d9e4332b28c86264b6e04021b6
0x11901 <wjk930726@live.com>
2019-02-28 18:30:19 +0800

    tagging release w.t.f.

--- test/Spec.hs
+++ test/Spec.hs
@@ -23,3 +23,10 @@ elem' :: (Eq a) => a -> [a] -> Bool
 elem' _ [] = False
 elem' a (x : xs) | a == x    = True
                  | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort []       = []
+quicksort (x : xs) = quicksort smallerOrEqual ++ [x] ++ quicksort larger
+  where
+    smallerOrEqual = [ a | a <- xs, a <= x ]
+    larger         = [ a | a <- xs, a > x ]
